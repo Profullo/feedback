efb47b71399f88f5571a34c1f6dd5b46dcd9a283
Nirupam Shuvo <39645954+NIRUPOM@users.noreply.github.com>
2021-01-05 15:21:55 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_USER","nirupom");
+define("DB_PASSWORD","123456");
+define("DB_NAME","contactform");
+$mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
+?>
---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <!--Add icon Link-->
+    <link rel="icon" href="images/profile_pic8.png" type="image" sizes="16x16">
+    <title>Profullo</title>
+  <!--Required meta tags-->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--Bootstrap CSS-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+  <!--Conect With CSS stylesheet -->
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<nav class="navbar" style="background-color: #006747;">
+	<div class="container">
+    <ul class="nav navbar-nav" style="color: #ffffff;">
+      <li><a href="https://www.profullo.com/">Home</a></li>
+      <li><a href="#about">About</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#clients">Our Clients</a></li>
+
+    </ul>
+</div>
+</nav>
+
+<div class="container">
+	<section class="about-area" id="about">
+	<div class="AboutUs">
+		<img src="images/banner.jpg" sizes="100px">
+		<br></br>
+		<p>
+			Profullo is a growing one-stop online shopping destination for Bangladesh. We are offering a completely hassle-free shopping experience through secure and trusted gateways. We offer you trendy and reliable shopping with all your favorite brands and more. Now shopping is easier, faster, and always joyous.
+			<br></br>
+			Profullo has been launched on July 30, 2020. It is an initiative of the IT firm TechnicalBind. Profullo showcases products from all categories such as clothing, footwear, jewelry, accessories, electronics, appliance, books, health & beauty, and still counting! Our collection combines the latest in fashion trends as well as the all-time favorites. Our products are exclusively selected for you. We, at Profullo, have all that you need under one umbrella.
+			<br></br>
+			In tune with the vision of Digital Bangladesh, Profullo opens the doorway for everybody to shop over the Internet. We constantly update with a lot of new products, services, and special offers. We provide on-time delivery of products and quick resolution of any concerns.
+			<br></br>
+			We provide our customers with a memorable online shopping experience. Our dedicated Profullo quality assurance team works round the clock to personally make sure the right packages reach on time. You can choose whatever you like. We deliver it right at your address across Bangladesh. Our services are at your doorsteps all the time. Get the best products with the best online shopping experience every time. You will enjoy online shopping here!
+			<br></br>
+			<br><b>Responsive Website</b></br>
+			Profullo offers you a flexible and responsive shopping experience. Whether you use a laptop, PC, or smartphone, you can shop from Profullo easily. Just order online, we will send your product to your doorstep very soon.
+			<br></br>
+			<br><b>Wide Range of Products</b></br>
+			Profullo showcases products from all categories such as ladies and gents' clothing, footwear, jewelry, accessories, electronics, health care, beauty products, and still counting! Our collection combines the latest in fashion trends as well as the all-time favorites. Various categories offer t-shirts, polo t-shirts, jeans/gabardine/twill pants, shirt, Panjabi, salwar-kameez, saree, Kurti, tops, leggings, innerwear, body spray & perfume, watches, shavers & trimmers, footwear, bags, jewelry, sunglasses, kids items, home decor, kitchen appliance, sanitary-fittings, gadget, foods items, gift items, leather products, health care and many more. Our products are exclusively selected for our valued customers.
+			<br></br>
+			<br><b>Quality Products</b></br>
+			We assure the quality of our products. We want you to avail the best quality products from the best sellers and brands in the country. Our dedicated Profullo quality assurance team works round the clock to personally make sure the right packages reach on time. Our quality assurance team makes sure that you are receiving the right product at the right price. You will find here the finest quality and premium brand products with reasonable pricing. We have a wide range of branded products of the varied category in our product line. We provide the best quality and service at the best price. So sign up now to get anything you need.
+			<br></br>
+			<br><b>Day Long Dedicated Customer Care</b></br>
+			Profullo customer care team is at your service from 10 AM to 10 PM every day all over the year. Our official hotline is +8801926814457. You can place an order, make any query about product and service, or track your order status. You will find our dedicated customer care team at all stages of your online shopping experience.
+			<br></br>
+			<br><b>Delivery All Over the Country</b></br>
+			Profullo opens the doorway for everybody to shop over the Internet. Our dedicated Profullo quality assurance team works round the clock personally to make sure the right packages reach on time. Our services are at your doorsteps with the lowest delivery charge. We process all deliveries through reputed courier service as well as our in house delivery team. We deliver products all over Bangladesh. If there is any modification in the delivery charge for a particular item, it will be mentioned in product details.
+			<br></br>
+			Order confirmation and delivery completion are subject to product availability. Delivery time may differ from one item to another. It can differ from standard delivery to beyond that.
+			<br></br>
+			Standard Delivery: If your delivery address is within Narayanganj city, products will be delivered within 2 business days. If it is outside Narayanganj then it will take 3-4 business days. If you order after 6 PM, it will be considered as an order of the next business day.
+			<br></br>
+			Note: Requested respected customers to refrain from buying excessive goods after being panicked. We are unable to entertain any bulk quantity during COVID-19. Delivery may delay due to COVID-19.
+			<br></br>
+			Our Business Day: Sunday to Thursday except for public holidays.
+			<br></br>
+			Exceptional Delivery: There are some exceptional items that we import from outside Bangladesh. These items can take 10 or more days to reach you. However, you will receive your order within the time specified.
+			<br></br>
+			Profullo does not deliver products outside Bangladesh. You can make your purchases on Profullo from anywhere in the world, but you can get delivery of the product only within Bangladesh.
+			<br></br>
+			<br><b>Easy Replacement</b></br>
+			We provide a great customer experience each time you shop with your Profullo. If you are not satisfied with your purchase, we ensure ‘Replacement Guarantee’. We will replace your purchased product if the product has any defect by its manufacturer or if the product is not the same one you ordered.
+			<br></br>
+			All you need to do is give us a call or drop an email at profullodotcom@gmail.com within 24 hours from the date of delivery. However, please return the product with the tags intact and in their original packaging, in an unwashed and undamaged condition. Replacement for products is subject to inspection and checking by Profullo team. Replacement cannot be possible if the product is burnt, damaged by a short circuit, or broken by the customer.
+			<br></br>
+			<br><b>Ensuring Your Privacy</b></br>
+			This privacy policy sets out how Profullo uses and protects any information that you give here when you use this website. We view the protection of your privacy as a very important principle. We are committed to ensuring your privacy here. Your information will only be used following this privacy statement whenever we ask you to provide any information by which you can be identified while using this website.
+			<br></br>
+			You will be required to enter a valid phone number and email while signing up and placing an order on Profullo. By registering your phone number and email with us, you consent to be contacted by us via email, phone calls, and/or SMS, in case of any order or delivery related updates. Profullo will not use your personal information to initiate any promotional phone call or SMS. We store and process your information in computers that are protected by physical as well as reasonable technological security measures.
+			<br></br>
+			Our primary goal in doing so is to provide you a safe, efficient, and customized experience. This allows us to provide services and features that most likely meet your needs. If you choose to buy on the website, we collect information about your buying behavior.
+			<br></br>
+			<br><b>Stay in Touch</b></br>
+			We would like to receive your feedback about our product, service, and site. Keep in contact with us and feel free to express any opinion, viewpoint, suggestion, or comment that you might have. You can call us at our hotline: +8801926814457 (10 AM to 10 PM); contact us via our official Facebook page.
+		</p>
+	</div>
+</div>
+<br></br>
+<div class="container"style="background-color: #F5F5F5";>
+<div class="header" style="text-align: center; ">
+	<section class="clients" id="clients">
+		<h3>Our Clients</h3>
+		<br></br>
+	<div class= col-md-2>
+		<img src="images/Aymper Markets.png">
+	</div>
+
+	<div class= col-md-2>
+		<img src="images/Celebrate Sweetnesss.jpg">
+	</div>
+
+	<div class= col-md-2>
+		<img src="images/Galaxia Estate Agencies.jpg">
+	</div>
+
+	<div class= col-md-2>
+		<img src="images/Oohlala Booths.png">
+	</div>
+
+	<div class= col-md-2>
+		<img src="images/Willenskraft.png">
+	</div>
+
+	<div class= col-md-2>
+		<img src="images/Click Digital.png">
+	</div>
+	<br></br>
+	</section>
+	
+</div>
+
+</div>
+
+<div class="container">
+	<h3>Please Submit feedback</h3>
+	<form action="form-process.php" method="POST">
+		<div class="form-group">
+			<label for="firstname">Firstname</label>
+			<input type="text" name="firstname" id="firstname" class="form-control" required>
+		</div>
+		<div class="form-group">
+			<label for="lastname">Lastname</label>
+			<input type="text" name="lastname" id="lastname" class="form-control" required>
+		</div>
+		<div class="form-group">
+			<label for="phone">Phone</label>
+			<input type="tel" name="phone" id="phone" class="form-control" required>
+		</div>
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email" class="form-control" required>
+		</div>
+		<div class="form-group">
+			<label for="message">Message</label>
+			<input type="text" name="message" id="message" class="form-control" required>
+		</div>
+		<div class="form-group">
+	<button class="btn btn-success" type="submit">Submit</button>
+	<button class="btn btn-danger" type="reset">Reset</button>
+</div>
+	</form>
+</div>
+ 
+<br></br>
+<div class="container">
+	<div class="row" style="background-color:	#F5F5F5;">
+		<div class="col-md-6">
+			<ul>
+				<h3>Legal and Privacy</h3>
+				<h4>
+					<br><a href="https://www.profullo.com/privacy-policy/">Privacy Policy</a></br>
+					<br><a href="https://www.profullo.com/terms-of-use/">Terms and Conditions</a></br>
+				</h4>
+			</ul>
+		</div> 
+
+
+		<div class="col-md-6">
+			<section class="contact-area" id="contact">
+				<div class="contact">
+					<ul>
+						<h3>Contact</h3>
+						<h4>
+				         <br><i class="fa fa-phone" aria-hidden="true"> +8801926814457 </i></br>
+				         <br><a href = "mailto: profullodotcom@gmail.com"> <i class="fa fa-envelope" aria-hidden="true"> profullodotcom@gmail.com </i> </a> </br>
+				         <br><i class="fa fa-map-marker" aria-hidden="true"> 53/A, South Sastapur, Upazila Road, Fatullah, Narayanganj 1420 </i></br></ul>
+				       <ul>
+				         <a href="https://www.facebook.com/profullodotcom"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+				         <a href="https://www.twitter.com/profullodotcom"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+				         <a href="https://www.instagram.com/profullobd"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				         <a href="https://www.linkedin.com/company/profullo-online-shopping"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				         <a href="https://github.com/Profullo/"><i class="fa fa-github" aria-hidden="true"></i></a></h4></ul>
+				     </div>
+				 </section>
+				</div>
+			</div>
+		</div>
+		  
+
+<!--footer section-->
+<div class="footer">
+   <p>&copy; Profullo 2020</p>
+</div>
+</body>
+</html>
